0eea359d0a33db1e13175d1052d147c58ed179f2
wds3817 <wds3817@gmail.com>
2020-04-19 03:31:21 -0800

    first commit

---
+++ src/Demo15.java
@@ -0,0 +1,46 @@
+import sun.reflect.generics.tree.Tree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Administrator on 2020/4/17.
+ * This is for the preorderTravesal by using the recursion method
+ */
+class TreeNode{
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) {
+        this.val = x;
+    }
+    TreeNode root;
+}
+
+public class Demo15 {
+    public static void main(String[] args) {
+        TreeNode node = new TreeNode(1);
+        node.right = new TreeNode(2);
+        node.right.left = new TreeNode(3);
+        node.right.left.left = new TreeNode(4);
+
+        Demo15 dm = new Demo15();
+        List<Integer> list;
+        list = dm.Preordertraversal(node);
+        for (int i = 0; i < list.size(); i++) {
+            System.out.print(list.get(i)+" ");
+        }
+    }
+    public List<Integer> Preordertraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        preordertraverse(root, res);
+        return res;
+    }
+    public void preordertraverse(TreeNode root, List<Integer> res) {
+        if (root == null) return;
+        res.add(root.val);
+        preordertraverse(root.left, res);
+        preordertraverse(root.right, res);
+    }
+
+}
