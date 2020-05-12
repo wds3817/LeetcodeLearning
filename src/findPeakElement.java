2a7ea045ddcbeeaec19043f290730738f5d09ca6
wds3817 <wds3817@gmail.com>
2020-05-12 18:30:09 +1000

    renewed codes

--- src/findPeakElement.java
+++ src/findPeakElement.java
@@ -6,8 +6,8 @@ public class findPeakElement {
         if (nums == null || nums.length == 0) {
             return 0;
         }
-        int start =1;
-        int end = nums.length - 2;
+        int start = 0;
+        int end = nums.length - 1;
         while (start + 1 < end) {
             int mid = start + (end - start) / 2;
             if (nums[mid] < nums[mid + 1]) {
