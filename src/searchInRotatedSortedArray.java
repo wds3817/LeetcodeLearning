d010bd22090665aec487fccb809575389037409d
wds3817 <wds3817@gmail.com>
2020-05-13 16:50:44 +1000

    search in rotated sorted array leetcode

---
+++ src/searchInRotatedSortedArray.java
@@ -0,0 +1,35 @@
+/**
+ * Created by Administrator on 2020/5/13.
+ */
+public class searchInRotatedSortedArray {
+    public int search(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int start = 0;
+        int end = nums.length - 1;
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if (nums[start] < nums[mid]) {
+                if (nums[start] <= target && target <= nums[mid]) {
+                    end = mid;
+                } else {
+                    start = mid;
+                }
+            } else {
+                if (nums[mid] <= target && target <= nums[end]) {
+                    start = mid;
+                } else {
+                    end = mid;
+                }
+            }
+        }
+        if (nums[start] == target) {
+            return start;
+        }
+        if (nums[end] == target) {
+            return end;
+        }
+        return -1;
+    }
+}
